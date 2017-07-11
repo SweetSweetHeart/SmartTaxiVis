ed7ea8b03fb2514aa452e807f579a25eb1e86ad9
Wang Qiru <1@wangqiru.com>
2017-07-11 18:29:15 +0100

    Added PU/DO type for Ajax

--- Development/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ Development/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -28,36 +28,39 @@ namespace WebApplication1.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.jsonArray = RetrieveTopZones(10);
+            //ViewBag.jsonArray = RetrieveTopZones(10);
             return View();
         }
 
-        public string[] RetrieveTopZones(int limit)
+        public string[] RetrieveTopZones(int limit, string type)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             Thread.Sleep(500);
-
-            string PUQuery = "call first_cursor("+ limit + ");";
-
+            string query;
+            if (type.Equals("PU"))
+                query = "call first_cursor_PU(" + limit + ");";
+            else if (type.Equals("DO"))
+                query = "call first_cursor_DO(" + limit + ");";
+            else return null;
 
             MySqlCommand cmd = conn.CreateCommand();
-            MySqlDataAdapter readPU = new MySqlDataAdapter(PUQuery, conn);
-            DataTable PUTable = new DataTable();
+            MySqlDataAdapter reader = new MySqlDataAdapter(query, conn);
+            DataTable table = new DataTable();
             conn.Open();
-            readPU.Fill(PUTable);
+            reader.Fill(table);
 
             var list = new List<JObject>();
-            long[] PUArray = new long[PUTable.Rows.Count];
-            long[] DOArray = new long[PUTable.Rows.Count];
-            string[] JSONArray = new string[PUTable.Rows.Count];
-            for (int i = 0; i < PUTable.Rows.Count; i++)
+            long[] PUArray = new long[table.Rows.Count];
+            long[] DOArray = new long[table.Rows.Count];
+            string[] JSONArray = new string[table.Rows.Count];
+            for (int i = 0; i < table.Rows.Count; i++)
             {
                 var pair = new JObject();
-                pair["name"] = PUTable.Rows[i][1].ToString();
+                pair["name"] = table.Rows[i][1].ToString();
                 list.Add(pair);
-                PUArray[i] = Convert.ToInt64(PUTable.Rows[i][2]);
-                DOArray[i] = Convert.ToInt64(PUTable.Rows[i][3]);
-                JSONArray[i] = PUTable.Rows[i][4].ToString();
+                PUArray[i] = Convert.ToInt64(table.Rows[i][2]);
+                DOArray[i] = Convert.ToInt64(table.Rows[i][3]);
+                JSONArray[i] = table.Rows[i][4].ToString();
             }
             conn.Close();
             //Debug.WriteLine(JsonConvert.SerializeObject(list));
@@ -73,9 +76,9 @@ namespace WebApplication1.Controllers
         //    return Json(jsonArray, JsonRequestBehavior.AllowGet);
         //}
 
-        public JsonResult AjaxRetrieveWithLimit(int limit)
+        public JsonResult AjaxRetrieveWithLimit(int limit, string type)
         {
-            string[] jsonArray = RetrieveTopZones(limit);
+            string[] jsonArray = RetrieveTopZones(limit, type);
             return Json(jsonArray);
         }
 
