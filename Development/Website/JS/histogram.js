3fa1055463520b7d52acf7d35ad56381d97afef8
Wang Qiru <1@wangqiru.com>
2017-08-07 17:26:53 +0100

    Dynamically adjusting histogram data

--- Development/Website/JS/histogram.js
+++ Development/Website/JS/histogram.js
@@ -1,64 +1,50 @@
+histogramData = [];
 
+function generateHistogramData() {
+    for (var i = 0; i < countT.length; i++) {
+        var trips = $.extend(true, [], countT[i]);
+        spliceMatrix(trips);
+        spliceSubMatrix(trips);
+        tripCount = getTripCount(trips);
+        if (i == time1) {
+            histogramData.push([i.toString(), tripCount, "#e74c3c"]);
+        }
+        else
+            histogramData.push([i.toString(), tripCount]);
+    }
+    drawHistogram();
+}
 
+function drawHistogram() {
+    // create a data set
+    $('#histogram').html("");
+    var data = anychart.data.set(histogramData);
 
-//The drawing of the histogram has been broken out from the data retrial 
-// or computation. (In this case the 'Irwin-Hall distribution' computation above)
+    // create a chart
+    histogramChart = anychart.column();
+    // create a column series and set the data
 
-function drawHistogram(){
+    var dataMap = data.mapAs({ x: [0], value: [1], fill: [2] });
 
-$("#histogram").empty()
-  data = [];
+    var series = histogramChart.column(dataMap);
 
-  for (var i = 0; i < 100; i++) {
-    var s = 0;
-      s += Math.random();
-    
-    data.push(s);
-  }
-//The drawing code needs to reference a responsive elements dimneions
-var width = $("#histogram").width();
-// var width = $(reference).empty().width(); we can chain for effeicanecy as jquery returns jquery.
+    // set the chart title
+    // chart.title("Histogram");
 
-var height = 250;  // We don't want the height to be responsive.
+    // set the padding between column groups
+    histogramChart.barGroupsPadding(0);
 
-var histogram = d3.layout.histogram() (data);
+    // set the titles of the axes
+    var xAxis = histogramChart.xAxis().title("Hour")
+    var yAxis = histogramChart.yAxis().title("Trips").orientation('right');
 
- 
-var x = d3.scale.ordinal()
-    .domain(histogram.map(function(d) { return d.x; }))
-    .rangeRoundBands([0, width]);
- 
-var y = d3.scale.linear()
-    .domain([0, d3.max(histogram.map(function(d) { return d.y; }))])
-    .range([0, height]);
+    histogramChart.contextMenu(false);
 
-xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0);
-
- 
-var svg = d3.select("#histogram").append("svg")
-    .attr("width", width)
-    .attr("height", 2 * height);
- 
-svg.selectAll("svg")
-    .data(histogram)
-  .enter().append("rect")
-    .attr("width", x.rangeBand())
-    .attr("x", function(d) { return x(d.x); })
-    .attr("y", function(d) { return height - y(d.y); })
-    .attr("height", function(d) { return y(d.y); });
-
- 
-svg.append("line")
-    .attr("x1", 0)
-    .attr("x2", width)
-    .attr("y1", height)
-    .attr("y2", height);
-  
+    histogramChart.listen("pointClick", function (e) {
+        time1 = e.pointIndex;
+        animationSetData();
+        toggleAnimation(true);
+    });
+    // set the container id
+    histogramChart.container("histogram").draw();
 };
-
-//Bind the window resize to the draw method.
-//A simple bind example is
-
-//$(window).resize(function() {
-//  draw_histogram(div_name, pos_data, neg_data);
-//});
--- Development/Website/JS/slider.js
+++ Development/Website/JS/slider.js
@@ -10,7 +10,7 @@
         connect: true,
         range: {
             'min': 1,
-            'max': limit
+            'max': totalZoneNum
         }
     });
 
@@ -37,13 +37,11 @@
 
     hourSlider.noUiSlider.on('change', function (values) {
         time1 = values[0];
-        if (time1 > 23)
-            time1 = 0;
         hour = time1;
         animationSetData();
         hour++;
         //time2 = values[1];
-        //retrieve(limit, type, time1, time2);
+        //retrieve(totalZoneNum, type, time1, time2);
         toggleAnimation(true);
         formatJSON();
     });
